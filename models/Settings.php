94a7488e583f7520f8f38df76f5716178496ca70
RomainMazB <romainmazb@gmail.com>
2021-01-01 01:38:58 +0100

    Fix an issue when you submit the settings form without defining a Currency and Currency Symbol: When saved, both are stored as empty strings which results in: - Config::get() returns an empty string instead of null, and the USD default is not picked anymore - Crashes both back and front-end (Cashier can't guess the currency)

--- models/Settings.php
+++ models/Settings.php
@@ -1,9 +1,17 @@
 <?php namespace OFFLINE\Cashier\Models;
 
 use Model;
+use October\Rain\Database\Traits\Validation;
 
 class Settings extends Model
 {
+    use Validation;
+
+    public $rules = [
+        'currency_currency' => 'required',
+        'currency_symbol' => 'required'
+    ];
+
     public $implement = ['System.Behaviors.SettingsModel'];
 
     // A unique code
